67822f2b331d91210b6a8692bfc95c08e51fc41a
YueChao <yue327305210@163.com>
2024-12-27 09:56:34 +0800

    init commit

---
+++ api-service/src/main/java/org/example/alvin/api/service/ApiServiceApplication.java
@@ -0,0 +1,11 @@
+package org.example.alvin.api.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ApiServiceApplication.class, args);
+  }
+}
---
+++ flyway-plus/src/main/java/org/example/alvin/flyway/plus/FlywayPlusApplication.java
@@ -0,0 +1,11 @@
+package org.example.alvin.flyway.plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayPlusApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(FlywayPlusApplication.class, args);
+  }
+}
